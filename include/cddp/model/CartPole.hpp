1edfec28217b1c49b31b959b851d482c1faf60a6
Tomo <tomohiro.sasaki@gatech.edu>
2024-03-11 15:30:25 -0400

    create and modify some models

---
+++ include/cddp/model/CartPole.hpp
@@ -0,0 +1,15 @@
+#ifndef CDDP_CARTPOLE_HPP
+#define CDDP_CARTPOLE_HPP
+
+#include <Eigen/Dense>
+#include <vector>
+#include "DynamicalSystem.hpp"
+
+namespace cddp {
+
+class CartPole : public cddp::DynamicalSystem {
+public:
+}
+} // namespace cddp
+
+#endif // CDDP_CARTPOLE_HPP
