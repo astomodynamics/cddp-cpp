6eb7849d40a0d5f84deba8292492df7bdb6f48e2
astomodynamics <tomo.sasaki.hiro@gmail.com>
2024-11-30 19:57:48 -0500

    update results

--- tests/dynamics_model/test_cartpole.cpp
+++ tests/dynamics_model/test_cartpole.cpp
@@ -64,104 +64,104 @@ TEST(CartPoleTest, DiscreteDynamics) {
     ASSERT_DOUBLE_EQ(cartpole.getTimestep(), 0.05);
     ASSERT_EQ(cartpole.getIntegrationType(), "rk4");
 
-    // Plot the results (position, velocity, angle, angular velocity, energy)
-    plt::figure();
-    plt::subplot(5, 1, 1);
-    plt::plot(time_data, x_data);
-    plt::title("Cart position");
-    plt::xlabel("Time [s]");
-    plt::ylabel("Position [m]");
+    // // Plot the results (position, velocity, angle, angular velocity, energy)
+    // plt::figure();
+    // plt::subplot(5, 1, 1);
+    // plt::plot(time_data, x_data);
+    // plt::title("Cart position");
+    // plt::xlabel("Time [s]");
+    // plt::ylabel("Position [m]");
 
-    plt::subplot(5, 1, 2);
-    plt::plot(time_data, theta_data);
-    plt::title("Pole angle");
-    plt::xlabel("Time [s]");
-    plt::ylabel("Angle [rad]");
+    // plt::subplot(5, 1, 2);
+    // plt::plot(time_data, theta_data);
+    // plt::title("Pole angle");
+    // plt::xlabel("Time [s]");
+    // plt::ylabel("Angle [rad]");
 
-    plt::subplot(5, 1, 3);
-    plt::plot(time_data, v_data);
-    plt::title("Cart velocity");
-    plt::xlabel("Time [s]");
-    plt::ylabel("Velocity [m/s]");
+    // plt::subplot(5, 1, 3);
+    // plt::plot(time_data, v_data);
+    // plt::title("Cart velocity");
+    // plt::xlabel("Time [s]");
+    // plt::ylabel("Velocity [m/s]");
 
-    plt::subplot(5, 1, 4);
-    plt::plot(time_data, w_data);
-    plt::title("Pole angular velocity");
-    plt::xlabel("Time [s]");
-    plt::ylabel("Angular velocity [rad/s]");
+    // plt::subplot(5, 1, 4);
+    // plt::plot(time_data, w_data);
+    // plt::title("Pole angular velocity");
+    // plt::xlabel("Time [s]");
+    // plt::ylabel("Angular velocity [rad/s]");
 
-    plt::subplot(5, 1, 5);
-    plt::plot(time_data, energy_data);
-    plt::title("Total energy");
-    plt::xlabel("Time [s]");
-    plt::ylabel("Energy [J]");
-    plt::save("../results/tests/cartpole_discrete_dynamics.png");
+    // plt::subplot(5, 1, 5);
+    // plt::plot(time_data, energy_data);
+    // plt::title("Total energy");
+    // plt::xlabel("Time [s]");
+    // plt::ylabel("Energy [J]");
+    // plt::save("../results/tests/cartpole_discrete_dynamics.png");
 
-    // Animation
-    plt::figure_size(800, 600);
-    plt::title("CartPole Animation");
-    plt::xlabel("x");
-    plt::ylabel("y");
+    // // Animation
+    // plt::figure_size(800, 600);
+    // plt::title("CartPole Animation");
+    // plt::xlabel("x");
+    // plt::ylabel("y");
 
-    double cart_width = 0.3;
-    double cart_height = 0.2;
-    double pole_width = 0.05;
-    double pole_length = cartpole.getPoleLength();
+    // double cart_width = 0.3;
+    // double cart_height = 0.2;
+    // double pole_width = 0.05;
+    // double pole_length = cartpole.getPoleLength();
 
-    state << 0.0, M_PI/3, 0.0, 0.0;  // Reset initial state
+    // state << 0.0, M_PI/3, 0.0, 0.0;  // Reset initial state
 
-    // create initial cartpole animation
-    for (int i = 0; i < num_steps + 1; ++i) {
-        if (i % 5 == 0) {
-            plt::clf();
+    // // create initial cartpole animation
+    // for (int i = 0; i < num_steps + 1; ++i) {
+    //     if (i % 5 == 0) {
+    //         plt::clf();
 
-            double x = state(0);
-            double theta = state(1);
+    //         double x = state(0);
+    //         double theta = state(1);
 
-            // Cart corners
-            std::vector<double> cart_x = {
-                x - cart_width/2, x + cart_width/2,
-                x + cart_width/2, x - cart_width/2,
-                x - cart_width/2
-            };
-            std::vector<double> cart_y = {
-                -cart_height/2, -cart_height/2,
-                cart_height/2, cart_height/2,
-                -cart_height/2
-            };
-            plt::plot(cart_x, cart_y, "k-");
+    //         // Cart corners
+    //         std::vector<double> cart_x = {
+    //             x - cart_width/2, x + cart_width/2,
+    //             x + cart_width/2, x - cart_width/2,
+    //             x - cart_width/2
+    //         };
+    //         std::vector<double> cart_y = {
+    //             -cart_height/2, -cart_height/2,
+    //             cart_height/2, cart_height/2,
+    //             -cart_height/2
+    //         };
+    //         plt::plot(cart_x, cart_y, "k-");
 
-            // Pole
-            double pole_end_x = x + pole_length * std::sin(theta);
-            double pole_end_y = -pole_length * std::cos(theta);
-            std::vector<double> pole_x = {x, pole_end_x};
-            std::vector<double> pole_y = {0, pole_end_y};
-            plt::plot(pole_x, pole_y, "b-");
+    //         // Pole
+    //         double pole_end_x = x + pole_length * std::sin(theta);
+    //         double pole_end_y = -pole_length * std::cos(theta);
+    //         std::vector<double> pole_x = {x, pole_end_x};
+    //         std::vector<double> pole_y = {0, pole_end_y};
+    //         plt::plot(pole_x, pole_y, "b-");
 
-            // Plot pole bob
-            std::vector<double> circle_x, circle_y;
-            for (int j = 0; j <= 20; ++j) {
-                double t = 2 * M_PI * j / 20;
-                circle_x.push_back(pole_end_x + pole_width * std::cos(t));
-                circle_y.push_back(pole_end_y + pole_width * std::sin(t));
-            }
-            plt::plot(circle_x, circle_y, "b-");
+    //         // Plot pole bob
+    //         std::vector<double> circle_x, circle_y;
+    //         for (int j = 0; j <= 20; ++j) {
+    //             double t = 2 * M_PI * j / 20;
+    //             circle_x.push_back(pole_end_x + pole_width * std::cos(t));
+    //             circle_y.push_back(pole_end_y + pole_width * std::sin(t));
+    //         }
+    //         plt::plot(circle_x, circle_y, "b-");
 
-            // Set fixed axis limits for stable animation
-            double view_width = 4.0;
-            plt::xlim(x - view_width/2, x + view_width/2);
-            plt::ylim(-view_width/2, view_width/2);
-            // plt::axis("equal");
+    //         // Set fixed axis limits for stable animation
+    //         double view_width = 4.0;
+    //         plt::xlim(x - view_width/2, x + view_width/2);
+    //         plt::ylim(-view_width/2, view_width/2);
+    //         // plt::axis("equal");
 
-            std::string filename = "../results/tests/cartpole_" + std::to_string(i) + ".png";
-            plt::save(filename);
-            plt::pause(0.01);
-        }
+    //         std::string filename = "../results/tests/cartpole_" + std::to_string(i) + ".png";
+    //         plt::save(filename);
+    //         plt::pause(0.01);
+    //     }
 
-        // Update state
-        state = cartpole.getDiscreteDynamics(state, Eigen::VectorXd::Zero(1));
+    //     // Update state
+    //     state = cartpole.getDiscreteDynamics(state, Eigen::VectorXd::Zero(1));
 
-    }
+    // }
 }
 
 // TEST(CartPoleTest, Jacobians) {
