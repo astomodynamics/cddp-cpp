be15cfaa2be8f789675596ec291fac709016a32e
Tomo <tomohiro.sasaki@gatech.edu>
2024-12-13 20:59:05 -0500

    Create twobody spacecraft model (#48)

---
+++ src/dynamics_model/spacecraft_twobody.cpp
@@ -0,0 +1,73 @@
+#include "dynamics_model/spacecraft_twobody.hpp"
+
+#include <cmath>
+#include <iostream>
+
+#include <Eigen/Dense>
+
+#include "cddp_core/helper.hpp"
+
+namespace cddp {
+
+SpacecraftTwobody::SpacecraftTwobody(double timestep, double mu, double mass)
+    : DynamicalSystem(STATE_DIM, CONTROL_DIM, timestep), mu_(mu), mass_(mass) {}
+
+Eigen::VectorXd SpacecraftTwobody::getContinuousDynamics(
+    const Eigen::VectorXd &state, const Eigen::VectorXd &control) const {
+  Eigen::VectorXd state_dot = Eigen::VectorXd::Zero(STATE_DIM);
+
+  const double x = state(STATE_X);
+  const double y = state(STATE_Y);
+  const double z = state(STATE_Z);
+  const double vx = state(STATE_VX);
+  const double vy = state(STATE_VY);
+  const double vz = state(STATE_VZ);
+
+  const double ux = control(CONTROL_UX);
+  const double uy = control(CONTROL_UY);
+  const double uz = control(CONTROL_UZ);
+
+  const double r = std::sqrt(x * x + y * y + z * z);
+  const double r3 = r * r * r;
+
+  // Position dynamics
+  state_dot(STATE_X) = vx;
+  state_dot(STATE_Y) = vy;
+  state_dot(STATE_Z) = vz;
+
+  // Velocity dynamics
+  state_dot(STATE_VX) = -mu_ * x / r3 + ux / mass_;
+  state_dot(STATE_VY) = -mu_ * y / r3 + uy / mass_;
+  state_dot(STATE_VZ) = -mu_ * z / r3 + uz / mass_;
+
+  return state_dot;
+}
+
+Eigen::MatrixXd SpacecraftTwobody::getStateJacobian(
+    const Eigen::VectorXd &state, const Eigen::VectorXd &control) const {
+  auto f = [&](const Eigen::VectorXd &x) {
+    return getContinuousDynamics(x, control);
+  };
+
+  return finite_difference_jacobian(f, state);
+}
+
+Eigen::MatrixXd SpacecraftTwobody::getControlJacobian(
+    const Eigen::VectorXd &state, const Eigen::VectorXd &control) const {
+  auto f = [&](const Eigen::VectorXd &u) {
+    return getContinuousDynamics(state, u);
+  };
+  return finite_difference_jacobian(f, control);
+}
+
+Eigen::MatrixXd SpacecraftTwobody::getStateHessian(
+    const Eigen::VectorXd &state, const Eigen::VectorXd &control) const {
+  return Eigen::MatrixXd::Zero(STATE_DIM * STATE_DIM, STATE_DIM);
+}
+
+Eigen::MatrixXd SpacecraftTwobody::getControlHessian(
+    const Eigen::VectorXd &state, const Eigen::VectorXd &control) const {
+  return Eigen::MatrixXd::Zero(STATE_DIM * CONTROL_DIM, CONTROL_DIM);
+}
+
+} // namespace cddp
