b752151f7721c85128c855e6fda345fdecde065f
Tomo Sasaki <tomo.sasaki.hiro@gmail.com>
2024-12-25 16:05:03 -0500

    Add command

--- examples/neural_dynamics/prepare_pendulum.cpp
+++ examples/neural_dynamics/prepare_pendulum.cpp
@@ -14,6 +14,9 @@
  limitations under the License.
 */
 
+// Build & run: 
+//   $ ./examples/prepare_pendulum [num_samples] [csv_filename]
+
 #include <iostream>
 #include <filesystem>
 #include <fstream>
